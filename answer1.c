d880401eb9a4954a9f445c561cf118a493ba6e88
william-tsang <514105868@qq.com>
2016-09-06 03:16:31 +0800

    for a test--“valid number” from leetcode
    Validate if a given string is numeric.
    
    Some examples:
    "0" => true
    " 0.1 " => true
    "abc" => false
    "1 a" => false
    "2e10" => true
    Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.

---
+++ answer1.c
@@ -0,0 +1,91 @@
+#define DEBUG 
+
+#ifdef DEBUG
+    #define RET_FALSE   ({\
+           printf("return FALSE at line:%d.\n", __LINE__);\
+            false;\
+        })
+    #define RET_TRUE    ({\
+            printf("return TRUE at line:%d.\n", __LINE__);\
+            true;\
+        })
+#else
+    #define RET_FALSE   false
+    #define RET_TRUE    true
+#endif
+
+#define SKIP_BLANK(p)   ({\
+        while (' ' == *(p))\
+            (p)++;\
+    })
+    
+#define BLANK_OR_NULL(p)   ({\
+        while (' ' == *(p))\
+            (p)++;\
+        (*(p))?(RET_FALSE):(RET_TRUE);\
+    })
+
+/* inter: "01" */    
+#define SKIP_INTER(p, cnt)    ({\
+        cnt = 0;\
+        while (('0'<=*(p)) && ('9'>=*(p)))\
+        {\
+            (p)++;\
+            (cnt)++;\
+        }\
+    })
+    
+/* flaot: "0.1" or "0." or ".1" or inter */
+#define SKIP_FLOAT(p, cnt)    ({\
+        if ('.' == *(p))\
+        {\
+            (p)++;\
+            SKIP_INTER((p), (cnt));\
+            if (cnt)    /* for such as ".0" or "12.12" */\
+                (cnt) ++;\
+        }\
+        else\
+        {\
+            SKIP_INTER((p), (cnt));\
+            if ((cnt) && ('.'==*(p)))\
+            {/* for such as "12." or "12.12" */\
+                (p)++;\
+                unsigned int    cnt_t = 0;\
+                SKIP_INTER((p), cnt_t);\
+                (cnt) += cnt_t+1;\
+            }\
+        }\
+    })
+    
+bool isNumber(char* s) 
+{
+    char            *p = s;
+    unsigned int    cnt = 0;
+    
+    if (!p || (!*p))
+        return RET_FALSE;
+        
+    SKIP_BLANK(p);
+    if (!*p)
+        return RET_FALSE;
+    if (('-'==*p) || ('+'==*p)) /* for such as "-2.1e+10" or "+2.1e-10" */
+        p ++;
+    
+    SKIP_FLOAT(p, cnt);
+    if (!cnt)
+        return RET_FALSE;
+        
+    if ('e' == *p)
+    {
+        p ++;
+        if (('-'==*p) || ('+'==*p))/* for such as "-2.1e+10" or "+2.1e-10" */
+            p ++;
+        SKIP_INTER(p, cnt);
+        if (cnt)
+            return BLANK_OR_NULL(p);
+    }
+    else
+        return BLANK_OR_NULL(p);
+        
+    return RET_FALSE;
+}
